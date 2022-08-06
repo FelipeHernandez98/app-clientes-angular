5e227b84bfda743f459c664256a173f7d11e7e39
unknown <andresfelipehc@ufps.edu.co>
2022-08-06 16:37:48 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+ import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'clientes-app';
+  curso: string = 'Curso de Angular con SpringBoot';
+  nombre: string = 'Felipe Hernandez';
+}
