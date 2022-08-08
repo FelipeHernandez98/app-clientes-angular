0dbde336e41a1ee114dedff82e9c137909d61e2b
unknown <andresfelipehc@ufps.edu.co>
2022-08-08 15:04:57 -0500

    implementando primera conexión entre bac springboot y front Angular

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,13 +2,17 @@
  import { Clientes } from './clientes.json'
  import { Cliente } from './cliente'
  import { of, Observable } from 'rxjs'
+ import { HttpClient } from '@angular/common/http'
 
  @Injectable()
  export class ClienteService{
 
-    constructor(){}
+private urlEndPoint:string = 'http://localhost:8080/api/clientes/listar';
+    constructor(private http: HttpClient){}
 
     getClientes(): Observable<Cliente[]> {
-        return of(Clientes); 
+        //return of(Clientes); 
+
+        return this.http.get<Cliente[]>(this.urlEndPoint);
     }
  }
