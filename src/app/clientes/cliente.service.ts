049f37154b82d6acdd286372c3da808599a1c548
unknown <andresfelipehc@ufps.edu.co>
2022-08-08 11:45:24 -0500

    usando clase de servicios ClienteService para inyección de dependencias en el componente clienets

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,7 +1,13 @@
  import { Injectable } from '@angular/core';
+ import { Clientes } from './clientes.json'
+ import { Cliente } from './cliente'
 
  @Injectable()
  export class ClienteService{
-    
+
     constructor(){}
+
+    getClientes(): Cliente[] {
+        return Clientes; 
+    }
  }
