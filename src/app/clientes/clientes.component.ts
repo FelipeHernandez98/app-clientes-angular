56667d911cc3bb25069a3c5ac78ab3f76ea40c95
unknown <andresfelipehc@ufps.edu.co>
2022-08-08 11:35:47 -0500

    creando un archivo aparte para cargar los datos de los clientes

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente'
+import { Clientes } from './clientes.json'
 
 @Component({
   selector: 'app-clientes',
@@ -7,25 +8,12 @@ import { Cliente } from './cliente'
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente [] = [
-    {id: 1, nombre: 'Felipe', apellido: 'Hernandez', email: 'felipe@correo.com', createAt: '2022-08-08'},
-    {id: 2, nombre: 'Angie', apellido: 'Rodriguez', email: 'angie@correo.com', createAt: '2022-08-08'},
-    {id: 3, nombre: 'Jose', apellido: 'Perez', email: 'jose@correo.com', createAt: '2022-08-08'},
-    {id: 4, nombre: 'Pepe', apellido: 'Gomez', email: 'pepe@correo.com', createAt: '2022-08-08'},
-    {id: 5, nombre: 'Pedro', apellido: 'Fernandez', email: 'pedro@correo.com', createAt: '2022-08-08'},
-    {id: 6, nombre: 'Marcos', apellido: 'Galeano', email: 'marcos@correo.com', createAt: '2022-08-08'},
-    {id: 7, nombre: 'Ignacio', apellido: 'Ordoñez', email: 'ignacio@correo.com', createAt: '2022-08-08'},
-    {id: 8, nombre: 'Patricio', apellido: 'Lizcano', email: 'patricio@correo.com', createAt: '2022-08-08'},
-    {id: 9, nombre: 'Laureano', apellido: 'Ramos', email: 'laur@correo.com', createAt: '2022-08-08'},
-    {id: 10, nombre: 'Facundo', apellido: 'Tarazona', email: 'facun@correo.com', createAt: '2022-08-08'},
-
-  
-  
-  ]
+  clientes: Cliente [] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.clientes = Clientes;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from './cliente'
+
+export const Clientes: Cliente [] = [
+    {id: 1, nombre: 'Felipe', apellido: 'Hernandez', email: 'felipe@correo.com', createAt: '2022-08-08'},
+    {id: 2, nombre: 'Angie', apellido: 'Rodriguez', email: 'angie@correo.com', createAt: '2022-08-08'},
+    {id: 3, nombre: 'Jose', apellido: 'Perez', email: 'jose@correo.com', createAt: '2022-08-08'},
+    {id: 4, nombre: 'Pepe', apellido: 'Gomez', email: 'pepe@correo.com', createAt: '2022-08-08'},
+    {id: 5, nombre: 'Pedro', apellido: 'Fernandez', email: 'pedro@correo.com', createAt: '2022-08-08'},
+    {id: 6, nombre: 'Marcos', apellido: 'Galeano', email: 'marcos@correo.com', createAt: '2022-08-08'},
+    {id: 7, nombre: 'Ignacio', apellido: 'Ordoñez', email: 'ignacio@correo.com', createAt: '2022-08-08'},
+    {id: 8, nombre: 'Patricio', apellido: 'Lizcano', email: 'patricio@correo.com', createAt: '2022-08-08'},
+    {id: 9, nombre: 'Laureano', apellido: 'Ramos', email: 'laur@correo.com', createAt: '2022-08-08'},
+    {id: 10, nombre: 'Facundo', apellido: 'Tarazona', email: 'facun@correo.com', createAt: '2022-08-08'},
+  ]
