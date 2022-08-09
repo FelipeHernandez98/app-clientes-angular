747abb469984dbe1f8447a1a653f679d3b9d94ee
unknown <andresfelipehc@ufps.edu.co>
2022-08-09 11:19:55 -0500

    implementando los metodos de crear cliente, y traer datos de cliente para editar, aplicando avisos de alertas personalizados

---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent implements OnInit {
+
+  titulo: string = 'Crear cliente';
+  public cliente: Cliente = new Cliente();
+
+  constructor(private clienteService: ClienteService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  public create(): void{
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    )
+  }
+ 
+}
