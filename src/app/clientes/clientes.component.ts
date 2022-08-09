cc422ef4d4d61d44420d27720a44182340582d5b
unknown <andresfelipehc@ufps.edu.co>
2022-08-09 11:59:59 -0500

    implementando el metodo de eliminar y completando el CRUD

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,22 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { Cliente } from './cliente'
-import { ClienteService } from './cliente.service';
-
+import { ClienteService } from './cliente.service'
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-clientes',
-  templateUrl: './clientes.component.html'
+  templateUrl: './clientes.component.html',
 })
 export class ClientesComponent implements OnInit {
+  clientes: Cliente[] = []
 
-  clientes: Cliente [] = [];
-
-  constructor(private clienteService: ClienteService) { }
+  constructor(private clienteService: ClienteService) {}
 
   ngOnInit(): void {
-    this.clienteService.getClientes().subscribe(
-      clientes => this.clientes = clientes
-    )
+    this.clienteService
+      .getClientes()
+      .subscribe((clientes) => (this.clientes = clientes))
   }
 
+  public delete(cliente: Cliente): void {
+    swal
+      .fire({
+        title: 'Estas seguro?',
+        text: 'Estas seguro de eliminar este uausrio',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminar!',
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.clienteService.delete(cliente.id).subscribe((response) => {
+            this.clientes = this.clientes.filter( cli => cli !==cliente)
+            swal.fire('Eliminado!', 'El usuario se ha eliminado.', 'success')
+          })
+        }
+      })
+  }
 }
